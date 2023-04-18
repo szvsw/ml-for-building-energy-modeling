9b21fe64939bdfadada4672519d6730effa8a7c3
Sam Wolk <mail@samwolk.info>
2023-04-17 22:17:07 -0700

    add mutation functions

--- schedule-ops/mutate.py
+++ schedule-ops/mutate.py
@@ -1,6 +1,7 @@
 import os
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 from functools import reduce
 
@@ -32,7 +33,22 @@ per sched: operations
 """
 
 operations = [
-    "roll","invert","scale","randomize","sine_0A","sine_0B","sine0C","sine_1A","sine_1B","sine_1C","synthetic","on","off" 
+    "reverse",
+    "roll",
+    "invert",
+    "scale",
+    "bias",
+    "noise",
+    "sin_overwrite",
+    "sin_bias",
+    "sin_0_amp",
+    "sin_0_freq",
+    "sin_0_phase",
+    "sin_1_amp",
+    "sin_1_freq",
+    "sin_1_phase",
+    "synthetic",
+    "on/off"
 ]
 
 op_indices = {name: i for i,name in enumerate(operations)}
@@ -54,18 +70,77 @@ def get_sched_values(template, path):
     assert 'fraction' in sched.Type.Name.lower()
     return sched.all_values
 
-def mutate_timeseries(series, operations):
-    for i in range(series.shape[0]):
-        """Handle rolling"""
-        roll = operations[i, op_indices["roll"]]
-        print(roll)
-        series[i] = np.roll(series[i], int(roll))
-    """Handle Inverting"""
-    invert = operations[:, op_indices["invert"]]
+def mutate_timeseries(series, operations, seed):
+    series = series.copy()
+    np.random.seed(seed)
+    n = 0
+    n_series = 0
+    if len(series.shape) > 1:
+        n = series.shape[1]
+        n_series = series.shape[0]
+    else:
+        n = series.shape[0]
+        n_series = 1
+    t_cycle = np.linspace(0, 2*np.pi, n)
+    for i in range(n_series):
+        rev           = operations[i, op_indices["reverse"]]
+        roll          = operations[i, op_indices["roll"]]
+        invert        = operations[i, op_indices["invert"]]
+        scale         = operations[i, op_indices["scale"]]
+        bias          = operations[i, op_indices["bias"]]
+        noise         = operations[i, op_indices["noise"]]
 
-    """Handle Scaling"""
-    scalars = operations[:, op_indices["scale"]]
-    series = series * scalars.reshape(-1,1)
+        sin_overwrite = operations[i, op_indices["sin_overwrite"]]
+        sin_bias      = operations[i, op_indices["sin_bias"]]
+        sin_0_amp     = operations[i, op_indices["sin_0_amp"]]
+        sin_0_freq    = operations[i, op_indices["sin_0_freq"]]
+        sin_0_phase   = operations[i, op_indices["sin_0_phase"]]
+        sin_1_amp     = operations[i, op_indices["sin_1_amp"]]
+        sin_1_freq    = operations[i, op_indices["sin_1_freq"]]
+        sin_1_phase   = operations[i, op_indices["sin_0_phase"]]
+
+        # TODO: Unimplemented
+        synthetic = operations[i, op_indices["synthetic"]]
+
+        on_off = operations[i, op_indices["on/off"]]
+
+        """Handle Reversing"""
+        if rev == 1:
+            series[i] = np.flip(series[i])
+
+        """Handle rolling"""
+        series[i] = np.roll(series[i], int(roll))
+
+        """Handle Inverting"""
+        if invert == 1:
+            series[i] = 1-series[i]
+        
+        """Handle Scaling"""
+        series[i] *= scale
+        series[i] = np.clip(series[i], 0, 1)
+
+        """Handle Biasing"""
+        series[i] += bias
+        series[i] = np.clip(series[i], 0, 1)
+
+
+        """Handle Noise"""
+        series[i] += np.random.rand(n)*noise
+        series[i] = np.clip(series[i], 0, 1)
+
+        """Handle Sine"""
+        if sin_overwrite == 1:
+            series[i] = 0
+
+        series[i] += sin_0_amp * (0.5*np.sin(sin_0_freq*(t_cycle + sin_0_phase))+0.5) + sin_1_amp * (0.5*np.sin(sin_1_freq*(t_cycle + sin_1_phase))+0.5) + sin_bias
+        series[i] = np.clip(series[i], 0, 1)
+
+        """Handle Consts"""
+        if on_off == 1:
+            series[i] = np.ones(n)
+        elif on_off == -1:
+            series[i] = np.zeros(n)
+
 
     return series
 
@@ -77,5 +152,27 @@ if __name__ == "__main__":
     templates = UmiTemplateLibrary.open(template_path)
     template = templates.BuildingTemplates[0]
 
-    x = np.array([[1,2,3], [4,5,6]])
-    print(mutate_timeseries(x, np.array([[0, 0, 1.5], [1, 0, 2]])))
+    zones = ["Perimeter"]
+    paths = [
+        ["Loads", "EquipmentAvailabilitySchedule"],
+        ["Loads", "LightsAvailabilitySchedule"]
+    ]
+    scheds = get_schedules(template,zones=zones, paths=paths)
+    print(scheds.shape)
+    # np.random.seed(1)
+    # x = np.random.rand(2,8760)
+    seed = 42
+    res = mutate_timeseries(
+        scheds, 
+        np.array([
+            [0, 0, 0, 1.0, 0.0, 0.00, 1, 0, 0.5, 1, 0, 0.25, 365, 0, 0, 0], 
+            [0, 0, 0, 0.0, 0.0, 1.00, 0, 0, 0.0, 0, 0, 0.0, 0.0, 0, 0, 0], 
+        ]),
+        seed
+    )
+    for i in range(scheds.shape[0]):
+        figs, axs = plt.subplots(1,2)
+        axs[0].plot(scheds[i, :7*24])
+        plt.title("Original/Mutated")
+        axs[1].plot(res[i, :7*24])
+    plt.show()
