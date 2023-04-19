73d2d6c77f78e70dcac3fc0e7434dcec1226edcd
szvsw <mail@samwolk.info>
2023-04-19 01:49:05 -0400

    rename main folder

---
+++ ml-for-bem/storage.py
@@ -0,0 +1,8 @@
+from google.cloud import storage
+
+storage_client = storage.Client.from_service_account_json("../credentials/bucket-key.json")
+bucket = storage_client.get_bucket("ml-for-bem-data")
+
+def upload_to_bucket(blob_name, file_name):
+    blob = bucket.blob(blob_name)
+    blob.upload_from_filename(file_name)
