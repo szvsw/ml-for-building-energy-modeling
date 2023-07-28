53c9cb2cc0a87da3f0808109d403501ce0b1bf79
Sam Wolk <36545842+szvsw@users.noreply.github.com>
2023-07-27 21:25:16 -0400

    implement target based calibration

--- ml-for-bem/calibrator.py
+++ ml-for-bem/calibrator.py
@@ -5,7 +5,7 @@ from schema import Schema
 from surrogate import Surrogate
 
 
-class Calibrator(nn.Module):
+class ShoeboxCalibrator(nn.Module):
     def __init__(
         self,
         ensemble_size: int,
@@ -13,8 +13,14 @@ class Calibrator(nn.Module):
         timeseries_input_period: int,
         timeseries_input_length: int,
         n_static_input_parameters: int,
-        weather_timeseries: torch.Tensor,
-        surrogate: Surrogate
+        surrogate: Surrogate,
+        width: float=4,
+        height: float=3,
+        depth: float=10,
+        orientation: int = 0,
+        epw_idx: int = 0,
+        roof_2_footprint: float = 0,
+        footprint_2_ground: float = 0,
     ):
         super().__init__()
         self.ensemble_size = ensemble_size
@@ -24,7 +30,11 @@ class Calibrator(nn.Module):
         self.n_static_input_parameters = n_static_input_parameters
 
         # Fixed shared weather vector
-        self.weather_timeseries = weather_timeseries
+        empty_batch = schema.generate_empty_storage_batch(n=2)
+        schema.update_storage_batch(storage_batch=empty_batch,parameter="base_epw", value=epw_idx)
+        schema.update_storage_batch(storage_batch=empty_batch,parameter="orientation", value=orientation)
+        ts = surrogate.get_batch_climate_timeseries(empty_batch)
+        self.weather_timeseries = torch.Tensor(ts[0]).to(device)
 
         # TODO: Use initializer constraints from feature engineering
         self._timeseries = nn.Parameter(
@@ -35,41 +45,90 @@ class Calibrator(nn.Module):
             ),
         )
 
+        self._perim_2_footprint = nn.Parameter(
+            torch.randn(self.ensemble_size, 1)
+        ) 
 
-
-
-
-        width = 4
-        height = 3
-        # Todo: build automatic parameter builder for geometric parameters etc, some are specified as "known" others as "free" others yet as "constrained", e.g.
-        self.height = surrogate.schema["height"].normalize(height) # assume 3m
-        self.width = surrogate.schema["width"].normalize(width) # assume 3m
-
-        # Should these be separate for the four directions? identical?  fixed?
-        
-        self._geo_ratios = nn.Parameter(
-            torch.randn(self.ensemble_size, 4)
-        ) # all four sides have the same adiabatic roof/ground ratios and core/perim split and depth
         self._wwr = nn.Parameter(
-            torch.randn(4*ensemble_size,1)
-        ) # unique for the four sides
-
-        # Create four orientations
-        orientations = torch.arange(4).reshape(-1, 1).tile((self.ensemble_size, 1)).to(device)
-        self.orientations = nn.functional.one_hot(orientations).squeeze()
+            torch.randn(ensemble_size,1)
+        ) 
 
         self._statics = nn.Parameter(
             torch.randn(ensemble_size, self.n_static_input_parameters),
         )
+
+
+
+
+        self.__width = width
+        self.__height = height
+        self.__depth = depth
+        self.__facade_2_footprint = self.__width / self.__depth
+        self.__roof_2_footprint = roof_2_footprint
+        self.__footprint_2_ground = footprint_2_ground
+
+        self.__orientation = orientation
+        self.orientation = nn.functional.one_hot(torch.arange(4))[self.__orientation].to(device)
+        # TODO: build automatic parameter builder for geometric parameters etc, some are specified as "known" others as "free" others yet as "constrained", e.g.
+
+        # Create four orientations
+        # orientations = torch.arange(4).reshape(-1, 1).tile((self.ensemble_size, 1)).to(device)
+        # self.orientations = nn.functional.one_hot(orientations).squeeze()
+
         self.surrogate = surrogate
+
+    @property
+    def width(self):
+        return self.surrogate.schema["width"].normalize(self.__width)
+
+    @property
+    def height(self):
+        return self.surrogate.schema["height"].normalize(self.__height)
+
+    @property
+    def facade_2_footprint(self):
+        return self.surrogate.schema["facade_2_footprint"].normalize(self.__facade_2_footprint)
+
+    @property
+    def roof_2_footprint(self):
+        return self.surrogate.schema["roof_2_footprint"].normalize(self.__roof_2_footprint)
+
+    @property
+    def footprint_2_ground(self):
+        return self.surrogate.schema["footprint_2_ground"].normalize(self.__footprint_2_ground)
+    
+    @property
+    def __area(self):
+        return self.__depth * self.__width
+    
+    @property
+    def area(self):
+        return (self.__area - self.surrogate.area_min) / (self.surrogate.area_max - self.surrogate.area_min)
+
+    @property
+    def __perim_area(self):
+        return self.__area * self.surrogate.schema["perim_2_footprint"].unnormalize(self.perim_2_footprint)
+
+    @property
+    def perim_area(self):
+        return (self.__perim_area - self.surrogate.area_perim_min) / (self.surrogate.area_perim_max - self.surrogate.area_perim_min)
+
+    @property
+    def __core_area(self):
+        return self.__area * (1-self.surrogate.schema["perim_2_footprint"].unnormalize(self.perim_2_footprint))
+
+    @property
+    def core_area(self):
+        return (self.__core_area - self.surrogate.area_core_min) / (self.surrogate.area_core_max - self.surrogate.area_core_min)
+
 
     @property
     def timeseries(self):
         return torch.sigmoid(self._timeseries)
     
     @property
-    def geo_ratios(self):
-        return torch.sigmoid(self._geo_ratios)
+    def perim_2_footprint(self):
+        return torch.sigmoid(self._perim_2_footprint)
 
     @property
     def wwr(self):
@@ -80,59 +139,36 @@ class Calibrator(nn.Module):
         return torch.sigmoid(self._statics)
 
     def forward(self):
+        widths = torch.Tensor((self.roof_2_footprint,)).repeat(self.ensemble_size).reshape(-1,1).to(device)
+        heights = torch.Tensor((self.roof_2_footprint,)).repeat(self.ensemble_size).reshape(-1,1).to(device)
+        f2f = torch.Tensor((self.facade_2_footprint,)).repeat(self.ensemble_size).reshape(-1,1).to(device)
+        # --->perim
+        r2f = torch.Tensor((self.roof_2_footprint,)).repeat(self.ensemble_size).reshape(-1,1).to(device)
+        f2g = torch.Tensor((self.footprint_2_ground,)).repeat(self.ensemble_size).reshape(-1,1).to(device)
+        # --> orient
+        orientations = self.orientation.reshape(1,4).repeat((self.ensemble_size,1)).to(device)
+        # --> statics
+        areas = torch.Tensor(self.area.repeat(self.ensemble_size).reshape(-1,1)).to(device)
+        perim_areas = torch.Tensor(self.perim_area).to(device)
+        core_areas = torch.Tensor(self.core_area).to(device)
+
+
         # ---------------------
         # insert any constant parameters which are not allowed to change, e.g. geometry.
-
-        # ---------------------
-        # repeat once per orientation
-        widths = self.width * torch.ones(4*self.ensemble_size,1).to(device)
-        heights = self.height * torch.ones(4*self.ensemble_size,1).to(device)
-        
-        _f2f = self.surrogate.schema["facade_2_footprint"].unnormalize(self.geo_ratios[:,0:1])
-        _p2f = self.surrogate.schema["perim_2_footprint"].unnormalize(self.geo_ratios[:,1:2])
-        _height = surrogate.schema["height"].unnormalize(self.height) 
-        _width = surrogate.schema["width"].unnormalize(self.width) 
-        _depths = _height / _f2f
-        _areas = _width * _depths
-        _perims = _areas * _p2f
-        _cores = _areas * (1-_p2f)
-        _perims = _areas * _p2f
-        areas_norm = (_areas - self.surrogate.area_min) / ( self.surrogate.area_max - self.surrogate.area_min)
-        perim_areas_norm = (_perims - self.surrogate.area_perim_min) / ( self.surrogate.area_perim_max - self.surrogate.area_perim_min)
-        core_areas_norm = (_cores - self.surrogate.area_core_min) / ( self.surrogate.area_core_max - self.surrogate.area_core_min)
-
-        areas_norm = areas_norm.repeat_interleave(4, axis=0)
-        perim_areas_norm = perim_areas_norm.repeat_interleave(4, axis=0)
-        core_areas_norm = core_areas_norm.repeat_interleave(4, axis=0)
-
-        # depths = geo_ratios[]
-        geo_ratios = self.geo_ratios.repeat_interleave(4,axis=0) # constrain to 0-1
-        statics = self.statics.repeat_interleave(4, axis=0)
-        statics[:,0] = schema["HeatingSetpoint"].normalize(19)
-        statics[:,1] = schema["CoolingSetpoint"].normalize(23)
-        statics[:,12] = (0.5 - 0.1) / (0.99 - 0.05) # force shgc to be 0.5
-        statics[:,13] = (0.7 - 0.1) / (0.99 - 0.05) # force shgc to be 0.5
+        self.statics[:,12] = (0.5 - 0.1) / (0.99 - 0.05) # force shgc to be 0.5
+        self.statics[:,13] = (0.7 - 0.1) / (0.99 - 0.05) # force vlt to be 0.7
 
 
         # <insert orientations and wwrs>
-        building_vector = torch.concatenate((widths,heights, geo_ratios, self.wwr, self.orientations, statics, areas_norm, perim_areas_norm, core_areas_norm), axis=1)
-
-        # <update any orientation responsive parameters>
-
-        # dummy vec
-
-        # -----------------
-        # expand building vector by adding final dimension and tiling
-        building_vector = building_vector.reshape(*building_vector.shape,1)
-        building_vector = building_vector.repeat_interleave(self.surrogate.output_resolution, axis=2)
+        building_vector = torch.concatenate((widths,heights, f2f,self.perim_2_footprint, r2f, f2g, self.wwr, orientations, self.statics, areas, perim_areas, core_areas), axis=1)
 
         # ---------------------
         # concatenate climate vectors and timeseries
         # (c, t) -> (1,c,t), c = weather file channels count, t=8760
         weather = self.weather_timeseries.reshape(-1, *self.weather_timeseries.shape)
 
-        # Repeat weather per ensemble member per orientation -> (4*ensemble_sice, c, t)
-        weather = torch.repeat_interleave(weather, 4 * self.ensemble_size, axis=0)
+        # Repeat weather per ensemble member
+        weather = torch.repeat_interleave(weather, self.ensemble_size, axis=0)
 
         # tile a week into a year long schedule for each candidate
         timeseries = self.timeseries.tile(
@@ -146,14 +182,12 @@ class Calibrator(nn.Module):
         # Trim to 8760
         timeseries = timeseries[:, :, : self.timeseries_input_length]
 
-        # repeat schedules per orientation
-        timeseries = timeseries.repeat_interleave(4, axis=0)
-
         # concatenate climate  and bldg timeseries
         timeseries_vector = torch.concatenate((weather, timeseries), axis=1)
 
         # Project timeseries to Latent vector
         latent_vect = self.surrogate.timeseries_net(timeseries_vector)
+        building_vector = building_vector.reshape(*building_vector.shape,1).repeat((1,1,self.surrogate.output_resolution))
 
         # concatenate
         x = torch.concatenate((latent_vect, building_vector), axis=1)
@@ -162,7 +196,7 @@ class Calibrator(nn.Module):
         # weight factors
 
         # ---------------------
-        return result, result.sum()
+        return result
 
 
 if __name__ == "__main__":
@@ -174,20 +208,34 @@ if __name__ == "__main__":
     # TODO: load in checkpoint and climate vector
     surrogate = Surrogate(schema=schema, checkpoint="batch_permute_lower_lr/batch_permute_lower_lr_202307250612_002_350000.pt")
 
+    result_idx = 500000
+    target = surrogate.results["eui_normalized"][result_idx]
+    vector = surrogate.full_storage_batch[result_idx]
+    orientation = schema["orientation"].extract_storage_values(vector).astype(int)
+    epw_idx = schema["base_epw"].extract_storage_values(vector).astype(int)
+    width = schema["width"].extract_storage_values(vector)
+    height = schema["height"].extract_storage_values(vector)
+    f2f = schema["facade_2_footprint"].extract_storage_values(vector)
+    r2f = schema["roof_2_footprint"].extract_storage_values(vector)
+    f2g = schema["footprint_2_ground"].extract_storage_values(vector)
+    depth = height/f2f
 
 
-    empty_batch = schema.generate_empty_storage_batch(n=2)
-    schema.update_storage_batch(storage_batch=empty_batch,parameter="base_epw", value=city_map["NY, New York"]["idx"])
-    ts = surrogate.get_batch_climate_timeseries(empty_batch)
-    print(surrogate.building_params_per_vector, surrogate.latent_size)
-    cal = Calibrator(
-        ensemble_size=10,
+
+    cal = ShoeboxCalibrator(
+        ensemble_size=25,
         n_timeseries_input_channels=3,
-        timeseries_input_period=24 * 7,
+        timeseries_input_period=24 * 14,
         timeseries_input_length=8760,
         n_static_input_parameters=14,
-        weather_timeseries=torch.Tensor(ts[0]).to(device),
-        surrogate=surrogate
+        surrogate=surrogate,
+        width=width,
+        height=height,
+        depth=depth,
+        roof_2_footprint=r2f,
+        footprint_2_ground=f2g,
+        orientation=orientation,
+        epw_idx=epw_idx,
     ).to(device)
 
     print(sum([param.numel() for param in cal.parameters()]))
@@ -201,46 +249,60 @@ if __name__ == "__main__":
 
 
     optim = torch.optim.Adam(cal.parameters(), lr=0.1)
+    loss_fn = nn.MSELoss()
+    target = torch.Tensor(target).unsqueeze(0).tile(cal.ensemble_size,1,1).to(device)
 
     res = None
-    for i in range(1000):
+    for i in range(500):
         optim.zero_grad()
-        res, sum = cal()
-        sum.backward()
+        result = cal()
+        loss = loss_fn(target,result)
+        loss.backward()
         optim.step()
-        print(sum.item())
-        if sum.item() < 0.1:
-            break
+        print(i, loss.item())
     
 
-    for i in range(10):
+    result = cal()
+    import matplotlib.pyplot as plt
+    fig, axs = plt.subplots(1,4)
+    for i in range(cal.ensemble_size):
+        for j in range(4):
+            tar = result[i,j]
+            if j == 0:
+                tar = tar*(surrogate.eui_perim_heating_max - surrogate.eui_perim_heating_min) + surrogate.eui_perim_heating_min
+            elif j == 1:
+                tar = tar*(surrogate.eui_perim_cooling_max - surrogate.eui_perim_cooling_min) + surrogate.eui_perim_cooling_min
+            elif j == 2:
+                tar = tar*(surrogate.eui_core_heating_max - surrogate.eui_core_heating_min) + surrogate.eui_core_heating_min
+            elif j == 3:
+                tar = tar*(surrogate.eui_core_cooling_max - surrogate.eui_core_cooling_min) + surrogate.eui_core_cooling_min
+            axs[j].plot(tar.detach().cpu().numpy(),"o", markersize=1, lw=0.6)
         print("-"*10 + str(i) + "-"*10)
-        ratios = cal.geo_ratios[i].detach().cpu().numpy()
-        f2f = ratios[0]
-        p2f = ratios[1]
-        r2f = ratios[2]
-        f2g = ratios[3]
+        f2f = cal.facade_2_footprint
+        r2f = cal.roof_2_footprint
+        f2g = cal.footprint_2_ground
 
-        wwr = cal.wwr.detach().cpu().numpy()[i*4:i*4+4].flatten()
+        p2f = cal.perim_2_footprint.detach().cpu().numpy()
+        wwr = cal.wwr[i].detach().cpu().numpy()[i*4:i*4+4].flatten()
 
-        statics = cal.statics.detach().cpu().numpy()
-        heatset = statics[i, 0]
-        coolset = statics[i, 1]
-        lpd = statics[i, 2]
-        epd = statics[i, 3]
-        pd = statics[i, 4]
-        inf = statics[i, 5]
-        facademass = statics[i, 6]
-        roofmass = statics[i, 7]
-        facader = statics[i, 8]
-        roofr = statics[i, 9]
-        slabr = statics[i, 10]
-        winu = statics[i, 11]
+        statics = cal.statics[i].detach().cpu().numpy()
+        heatset = statics[0]
+        coolset = statics[1]
+        lpd = statics[2]
+        epd = statics[3]
+        pd = statics[4]
+        inf = statics[5]
+        facademass = statics[6]
+        roofmass = statics[7]
+        facader = statics[8]
+        roofr = statics[9]
+        slabr = statics[10]
+        winu = statics[11]
         
-        print("f2f",schema["facade_2_footprint"].unnormalize(f2f))
+        # print("f2f",schema["facade_2_footprint"].unnormalize(f2f))
         print("p2f",schema["perim_2_footprint"].unnormalize(p2f))
-        print("r2f",schema["roof_2_footprint"].unnormalize(r2f))
-        print("f2g",schema["footprint_2_ground"].unnormalize(f2g))
+        # print("r2f",schema["roof_2_footprint"].unnormalize(r2f))
+        # print("f2g",schema["footprint_2_ground"].unnormalize(f2g))
         print("wwr",schema["wwr"].unnormalize(wwr))
         print("hsp", schema["HeatingSetpoint"].unnormalize(heatset))
         print("csp", schema["CoolingSetpoint"].unnormalize(coolset))
@@ -256,10 +318,21 @@ if __name__ == "__main__":
         print("winu", winu*(schema["WindowSettings"].max[0] -schema["WindowSettings"].min[0])+schema["WindowSettings"].min[0])
 
         print("\n")
-        print(res[i].detach().cpu().numpy())
-        if i == 9:
+        if i == 0:
             scheds = cal.timeseries[i].detach().cpu().numpy()
-            print(scheds.shape)
         print("\n")
+    for i in range(4):
+        tar = target[0,i]
+        if i == 0:
+            tar = tar*(surrogate.eui_perim_heating_max - surrogate.eui_perim_heating_min) + surrogate.eui_perim_heating_min
+        elif i == 1:
+            tar = tar*(surrogate.eui_perim_cooling_max - surrogate.eui_perim_cooling_min) + surrogate.eui_perim_cooling_min
+        elif i == 2:
+            tar = tar*(surrogate.eui_core_heating_max - surrogate.eui_core_heating_min) + surrogate.eui_core_heating_min
+        elif i == 3:
+            tar = tar*(surrogate.eui_core_cooling_max - surrogate.eui_core_cooling_min) + surrogate.eui_core_cooling_min
+        axs[i].plot(tar.detach().cpu().numpy(),"o",markersize=1, linestyle="dashed", lw=1.5, alpha=0.5)
+    fig.tight_layout()
+    plt.show()
 
 
