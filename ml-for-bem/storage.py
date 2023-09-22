49e85590a7ff973bf5618fcc3d99ec11744d0a84
Sam Wolk <36545842+szvsw@users.noreply.github.com>
2023-09-22 12:38:48 -0400

    update storage to load client from env

--- ml-for-bem/storage.py
+++ ml-for-bem/storage.py
@@ -1,23 +1,42 @@
 import os
+import json
 import shutil
 import logging
 
 from pathlib import Path
 
 from google.cloud import storage
+from dotenv import load_dotenv
+
+load_dotenv()
 
 logging.basicConfig()
 logger = logging.getLogger("Storage")
 logger.setLevel(logging.INFO)
 
-storage_client = storage.Client.from_service_account_json(
-    Path(os.path.dirname(os.path.abspath(__file__)))
-    / ".."
-    / "credentials"
-    / "bucket-key.json"
-)
 
-bucket = storage_client.get_bucket("ml-for-bem-data")
+try:
+    creds = json.loads(os.getenv("GOOGLE_APPLICATION_CREDENTIALS"))
+    storage_client = storage.Client.from_service_account_info(creds)
+    logger.info("Successfully opened GCS client.")
+except BaseException as e:
+    logger.error("ERR", exc_info=e)
+    logger.warning(
+        "Could not find GCS credentials in system env, falling back to json."
+    )
+    storage_client = storage.Client.from_service_account_json(
+        Path(os.path.dirname(os.path.abspath(__file__)))
+        / ".."
+        / "credentials"
+        / "bucket-key.json"
+    )
+
+try:
+    bucket = storage_client.get_bucket("ml-for-bem-data")
+    logger.info("Successfully fetched bucket location")
+except BaseException as e:
+    logger.error("Error fetching bucket location", exc_info=e)
+    raise e
 
 
 def check_bucket_completeness():
@@ -62,15 +81,15 @@ def download_epws():
         Path(os.path.dirname(os.path.abspath(__file__)))
         / "data"
         / "epws"
-        / "city_epws_indexed.zip"
+        / "global_epws_indexed.zip"
     )
     unzip_folder = (
         Path(os.path.dirname(os.path.abspath(__file__)))
         / "data"
         / "epws"
-        / "city_epws_indexed"
+        / "global_epws_indexed"
     )
-    download_from_bucket("epws/city_epws_indexed.zip", zip_path)
+    download_from_bucket("epws/global_epws_indexed.zip", zip_path)
     logger.info("Unzipping EPWs...")
     os.makedirs(unzip_folder, exist_ok=True)
     shutil.unpack_archive(zip_path, unzip_folder)
