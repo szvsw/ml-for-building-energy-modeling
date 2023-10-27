53b61af7fac16e4a8d17fd7f4fec2267522c7fd0
Sam Wolk <36545842+szvsw@users.noreply.github.com>
2023-10-27 19:23:31 -0400

    finish relocation of schedule_from_seed

--- ml-for-bem/shoeboxer/builder.py
+++ ml-for-bem/shoeboxer/builder.py
@@ -37,39 +37,11 @@ import pandas as pd
 
 
 EPW_PATH = Path(module_path, EPW_RELATIVE_PATH)
-SCHEDULES_ARR_PATH = Path(module_path, DEFAULT_SCHEDULES_PATH)
 
 logging.basicConfig()
 logger = logging.getLogger("ShoeBox")
 # logger.setLevel(logging.DEBUG)
 logger.setLevel(logging.INFO)
-
-default_schedules = np.load(SCHEDULES_ARR_PATH)
-
-
-def schedules_from_seed(seed: int):
-    # step 1: load the seedo
-    np.random.seed(int(seed))
-
-    # step 2: decide whether or not to use random or schedules
-    is_fully_rand = np.random.choice([True, False], size=1, p=[0.8, 0.2])
-
-    # step 3: if is fully_rand, then use random, else use schedules
-    if is_fully_rand:
-        scheds = np.random.rand(3, 8760)
-    else:
-        # Step 3a. select which archetype to use
-        sched_type = np.random.choice(range(default_schedules.shape[0]))
-        # Step 3b. select schedules
-        scheds = default_schedules[sched_type]
-        # Step 3c. mutate schedules with nosie
-        noise_amp = np.random.rand(3) * 0.25
-        noise = np.random.rand(3, 8760) * noise_amp.reshape(3, 1)
-        scheds = scheds + noise
-        # step 3d. clip to 0-1
-        scheds = np.clip(scheds, 0, 1)
-
-    return scheds
 
 
 def day_to_epbunch(dsched, idx=0, sched_lim=sched_type_limits):
@@ -270,8 +242,8 @@ class ShoeBox:
             roof_r_val=schema["RoofRValue"].extract_storage_values(vector),
             roof_mass=schema["RoofMass"].extract_storage_values(vector),
             slab_r_val=schema["SlabRValue"].extract_storage_values(vector),
-            shgc=schema["WindowUValue"].extract_storage_values(vector),
-            window_u_val=schema["WindowShgc"].extract_storage_values(vector),
+            shgc=schema["WindowShgc"].extract_storage_values(vector),
+            window_u_val=schema["WindowUValue"].extract_storage_values(vector),
             # visible_transmittance=0.8,  # TODO?
         )
         return cls(
