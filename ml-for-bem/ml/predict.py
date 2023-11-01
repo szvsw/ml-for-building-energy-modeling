d70000af28ba7fe8c80e49182df0fd7a3c8f2e37
Sam Wolk <36545842+szvsw@users.noreply.github.com>
2023-11-01 09:53:13 -0400

    add model consumption handler

--- ml-for-bem/ml/predict.py
+++ ml-for-bem/ml/predict.py
@@ -1,10 +1,40 @@
+from pathlib import Path
 import pandas as pd
 import numpy as np
+import wandb
 from lightning.pytorch import Trainer
 from torch.utils.data import DataLoader
 
 from ml.train import Surrogate
 from ml.data import PredictBuildingDataset
+
+
+def fetch_model(
+    registry="ml-for-building-energy-modeling/model-registry",
+    model: str = "Global UBEM Shoebox Surrogate with Combined TS Embedder",
+    tag: str = "latest",
+    resource: str = "model.ckpt",
+) -> Surrogate:
+    """
+    Fetches a surrogate model from the W&B cloud.
+
+    Args:
+        registry (str): The W&B registry to fetch the model from.
+        model (str): The model name.
+        tag (str): The model tag.
+        resource (str): The file resource to fetch from within the model artifact.
+
+    Returns:
+        surrogate (Surrogate): The surrogate model.
+    """
+    api = wandb.Api()
+    local_dir = Path("data") / "models" / tag
+    model_str = f"{registry}/{model}:{tag}"
+    surrogate_artifact = api.artifact(model_str, type="model")
+    pth = surrogate_artifact.get_path(resource)
+    model_path = pth.download(local_dir)
+    surrogate = Surrogate.load_from_checkpoint(model_path)
+    return surrogate
 
 
 # TODO: Make sure that climate array is transformed
