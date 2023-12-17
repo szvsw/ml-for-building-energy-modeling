d560f8d15e2dc4a15ec185622261918791db53c3
Sam Wolk <36545842+szvsw@users.noreply.github.com>
2023-12-17 10:07:36 -0500

    Improve push/pull scaffold

--- ml-for-bem/idfworker/pull.py
+++ ml-for-bem/idfworker/pull.py
@@ -6,18 +6,27 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def receive_messages(queue_url, num_msgs=1, wait_time=1, visibility_timeout=120):
+def receive_n_messages(
+    sqs_client,
+    queue_url,
+    num_msgs=1,
+    wait_time=1,
+    visibility_timeout=120,
+):
     """
     Receive messages from SQS queue.
 
     Args:
+        sqs_client: The SQS client.
         queue_url: The URL of the SQS queue.
-        num_msgs: The number of messages to receive (default: 10).
-        wait_time: The time in seconds to wait for messages (default: 20).
+        num_msgs: The number of messages to receive (default: 1).
+        wait_time: The time in seconds to wait for messages (default: 1).
         visibility_timeout: The time in seconds to make received messages
-            invisible to other consumers (default: 30).
+            invisible to other consumers (default: 120).
+
+    Returns:
+        The SQS response.
     """
-    sqs_client = boto3.client("sqs")
     return sqs_client.receive_message(
         QueueUrl=queue_url,
         MaxNumberOfMessages=num_msgs,
@@ -27,31 +36,132 @@ def receive_messages(queue_url, num_msgs=1, wait_time=1, visibility_timeout=120)
     )
 
 
-def delete_message(queue_url, receipt_handle):
-    sqs_client = boto3.client("sqs")
+def delete_message(sqs_client, queue_url, receipt_handle):
+    """
+    Delete message from SQS queue.
+
+    Args:
+        sqs_client: The SQS client.
+        queue_url: The URL of the SQS queue.
+        receipt_handle: The receipt handle of the message to delete.
+
+    Returns:
+        None
+    """
     sqs_client.delete_message(QueueUrl=queue_url, ReceiptHandle=receipt_handle)
 
 
-def process_message(message):
-    # Placeholder for processing logic
-    print("Processing message:", message)
+def reset_message_visibility(
+    sqs_client, queue_url, receipt_handle, visibility_timeout=120
+):
+    """
+    Reset message visibility timeout.
+
+    Args:
+        sqs_client: The SQS client.
+        queue_url: The URL of the SQS queue.
+        receipt_handle: The receipt handle of the message to reset.
+        visibility_timeout: The time in seconds to make received messages
+            invisible to other consumers (default: 120).
+
+    Returns:
+        None
+    """
+
+    sqs_client.change_message_visibility(
+        QueueUrl=queue_url,
+        ReceiptHandle=receipt_handle,
+        VisibilityTimeout=visibility_timeout,
+    )
+
+
+def process_message(message, handler=None):
+    """
+    Process message.
+
+    Args:
+        message: The message to process.
+        handler (callable, default=None): The function to handle the message.  If none, just prints the message.
+
+    Returns:
+        The result of the handler (or None if not specified).
+    """
+    if handler:
+        return handler(message)
+    else:
+        logger.warning(f"No handler specified for message:")
+        logger.warning(message)
+        return None
 
 
 @click.command()
-@click.option("--bucket", prompt="Bucket name", help="The name of the S3 bucket.")
-@click.option("--queue", prompt="Queue name", help="The name of the SQS queue.")
 @click.option(
-    "--experiment", prompt="Experiment name", help="The name of the experiment."
+    "--queue",
+    prompt="Queue name",
+    help="The name of the SQS queue.",
+    default="ml-for-bem,",
 )
-@click.option("--batch_id", prompt="Batch ID", help="The Batch ID to filter messages.")
-@click.option("--n_messages", prompt="Number of messages", help="Number of messages.")
-def consume_messages(bucket, queue, experiment, batch_id, n_messages):
+@click.option(
+    "--experiment",
+    prompt="Experiment name",
+    help="The name of the experiment.",
+)
+@click.option(
+    "--batch_id",
+    prompt="Batch ID",
+    help="The Batch ID to filter messages.",
+    default="idf/batch/test",
+)
+@click.option(
+    "--num_messages_to_process",
+    prompt="Number of messages",
+    help="Number of messages.",
+    default=10,
+)
+@click.option(
+    "--visibility_timeout",
+    default=120,
+    help="Visibility timeout in seconds.",
+)
+@click.option(
+    "--wait_time",
+    default=1,
+    help="Time in seconds to wait for messages to arrive.",
+)
+@click.option(
+    "--num_msgs_per_request",
+    default=10,
+    help="Number of messages to receive per queue request.",
+)
+def consume_messages(
+    queue,
+    experiment,
+    batch_id,
+    num_messages_to_process,
+    num_msgs_per_request,
+    visibility_timeout,
+    wait_time,
+    handler=None,
+):
     sqs_client = boto3.client("sqs")
     queue_url = sqs_client.get_queue_url(QueueName=queue)["QueueUrl"]
 
-    for i in range(int(n_messages)):
-        logger.info(f"{i:03d}: fetching messages from SQS")
-        response = receive_messages(queue_url)
+    processed_messages = 0
+    attempts_made = 0
+    results = []
+
+    while processed_messages < int(num_messages_to_process) and attempts_made < 3 * int(
+        num_messages_to_process
+    ):
+        logger.info(f"--- Attempt {attempts_made:03d} ---")
+        logger.info(f"fetching messages from SQS")
+        response = receive_n_messages(
+            sqs_client=sqs_client,
+            queue_url=queue_url,
+            num_msgs=num_msgs_per_request,
+            wait_time=wait_time,
+            visibility_timeout=visibility_timeout,
+        )
         messages = response.get("Messages", [])
         logger.info(f"Received {len(messages)} messages from SQS queue {queue_url}")
 
@@ -62,8 +172,15 @@ def consume_messages(bucket, queue, experiment, batch_id, n_messages):
                 and attrs.get("BatchId", {}).get("StringValue") == batch_id
             ):
                 logger.info(f"Processing message: {msg['MessageId']}")
-                process_message(msg["Body"])
-                delete_message(queue_url, msg["ReceiptHandle"])
+                result = process_message(message=msg["Body"], handler=handler)
+                results.append(result)
+                delete_message(
+                    sqs_client=sqs_client,
+                    queue_url=queue_url,
+                    receipt_handle=msg["ReceiptHandle"],
+                )
+                processed_messages += 1
+                logger.info(f"Processed {processed_messages} messages")
             else:
                 logger.warning(f"Skipping message: {msg['MessageId']}")
                 logger.warning(
@@ -72,6 +189,18 @@ def consume_messages(bucket, queue, experiment, batch_id, n_messages):
                 logger.warning(
                     f"Expected BatchId: {batch_id}, but got {attrs.get('BatchId', {}).get('StringValue')}"
                 )
+                reset_message_visibility(
+                    sqs_client=sqs_client,
+                    queue_url=queue_url,
+                    receipt_handle=msg["ReceiptHandle"],
+                    visibility_timeout=0,
+                )
+            attempts_made += 1
+    logger.info(
+        f"Finished processing messages, processed {processed_messages} messages"
+    )
+
+    return results
 
 
 if __name__ == "__main__":
--- ml-for-bem/idfworker/push.py
+++ ml-for-bem/idfworker/push.py
@@ -1,7 +1,12 @@
 import os
+import logging
+import json
 import uuid
 import boto3
 import click
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 def generate_uuid():
@@ -11,21 +16,37 @@ def generate_uuid():
 def create_sqs_queue_if_not_exists(sqs_client, queue_name):
     try:
         response = sqs_client.create_queue(QueueName=queue_name)
+        logger.info(f"Created SQS queue {queue_name}")
         return response["QueueUrl"]
     except sqs_client.exceptions.QueueNameExists:
+        logger.info(f"SQS queue {queue_name} already exists, fetching.")
         return sqs_client.get_queue_url(QueueName=queue_name)["QueueUrl"]
 
 
-def upload_to_s3(bucket, experiment, batch_id, file_path):
-    s3_client = boto3.client("s3")
-    file_name = os.path.basename(file_path)
-    s3_key = f"{experiment}/{batch_id}/{file_name}"
+def check_if_s3_file_exists(s3_client, bucket, key):
+    try:
+        s3_client.head_object(Bucket=bucket, Key=key)
+        logger.info(f"File {key} exists in bucket {bucket}")
+        return True
+    except s3_client.exceptions.ClientError:
+        logger.info(f"File {key} does not exist in bucket {bucket}")
+        return False
+
+
+def construct_s3_key(experiment, batch_id, file):
+    return f"{experiment}/{batch_id}/{os.path.basename(file)}"
+
+
+def upload_to_s3(s3_client, bucket, experiment, batch_id, file_path):
+    logger.info(f"Uploading {file_path} to S3...")
+    s3_key = construct_s3_key(experiment, batch_id, file_path)
     s3_client.upload_file(file_path, bucket, s3_key)
-    return f"s3://{bucket}/{s3_key}"
+    logger.info(f"Uploaded {file_path} to S3")
+    return s3_key
 
 
-def send_to_sqs(queue_url, s3_path, batch_id, experiment):
-    sqs_client = boto3.client("sqs")
+def send_to_sqs(sqs_client, queue_url, s3_path, batch_id, experiment):
+    logger.info(f"Sending message to SQS queue {queue_url}")
     sqs_client.send_message(
         QueueUrl=queue_url,
         MessageBody=s3_path,
@@ -45,28 +66,44 @@ def send_to_sqs(queue_url, s3_path, batch_id, experiment):
 @click.option(
     "--folder", prompt="Folder path", help="The path to the folder with .idf files."
 )
+@click.option("--epw", prompt="EPW path", help="The path to the EPW file.")
+@click.option(
+    "--batch_id",
+    default=None,
+    help="The Batch ID to filter messages.",
+    required=False,
+)
 def process_files(
     bucket,
     experiment,
     queue,
     folder,
+    epw,
+    batch_id,
 ):
-    batch_id = generate_uuid()
     sqs_client = boto3.client("sqs")
+    s3_client = boto3.client("s3")
+    batch_id = generate_uuid() if batch_id is None else batch_id
     queue_url = create_sqs_queue_if_not_exists(sqs_client, queue)
 
     for file_name in os.listdir(folder):
-        if file_name.endswith(".idf"):
-            file_path = os.path.join(folder, file_name)
-            s3_path = upload_to_s3(bucket, experiment, batch_id, file_path)
-            send_to_sqs(queue_url, s3_path, batch_id, experiment)
+        if file_name.endswith(".idf") or file_name.endswith(".epjson"):
+            idf_file_path = os.path.join(folder, file_name)
+            idf_s3_path = upload_to_s3(
+                s3_client, bucket, experiment, batch_id, idf_file_path
+            )
+            if not check_if_s3_file_exists(
+                s3_client, bucket, construct_s3_key(experiment, batch_id, epw)
+            ):
+                epw_s3_path = upload_to_s3(s3_client, bucket, experiment, batch_id, epw)
+            else:
+                epw_s3_path = construct_s3_key(experiment, batch_id, epw)
+            data = {"idf": idf_s3_path, "epw": epw_s3_path, "bucket": bucket}
+            data = json.dumps(data)
+            send_to_sqs(sqs_client, queue_url, data, batch_id, experiment)
 
     print(f"Batch ID: {batch_id}")
 
 
 if __name__ == "__main__":
-    """
-    To Run:
-    python orchestrate.py --bucket <bucket_name> --experiment <experiment_name> --queue <queue_name> --folder <folder_path>
-    """
     process_files()
